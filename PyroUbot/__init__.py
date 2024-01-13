1fa84b2877344d59aad3c913b53e99cd5691b0b4
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2024-01-13 11:18:33 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -106,6 +106,7 @@ class Ubot(Client):
     _get_my_id = []
     _translate = {}
     _get_my_peer = {}
+    _group_call = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, device_model="ᴍᴀxɢᴜɴs ᴜʙᴏᴛ")
