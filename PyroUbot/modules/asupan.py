bf3e1af68c7cbb86e68f22e0d166962b2def9bb3
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2024-01-17 21:15:16 +0700

    Update asupan.py

--- PyroUbot/modules/asupan.py
+++ PyroUbot/modules/asupan.py
@@ -1,1 +1,34 @@
+from PyroUbot import *
 
+
+__MODULE__ = "asupan"
+__HELP__ = f"""
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴀsᴜᴘᴘᴀɴ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}asupan</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ɢᴀʙᴜᴛ ᴅᴏᴀɴɢ ɪɴɪ 
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}pap</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴅᴀᴘᴀᴛᴋᴀɴ ᴘᴀᴘ ʙᴜɢɪʟ ᴅᴀʀɪ ᴛᴜʜᴀɴ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}bokep</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ ʙᴏᴋᴇᴘ  ʙɪᴀʀ ᴜɴᴛᴜᴋ ᴄᴏʟɪ  ᴅᴀɴ ᴄᴏʟᴍᴇᴋ
+  """
+
+
+@PY.UBOT("asupan", sudo=True)
+@PY.TOP_CMD
+async def _(client, message):
+    await _(client, message)
+
+
+@PY.UBOT("pap", sudo=True)
+@PY.TOP_CMD
+async def _(client, message):
+    await bugil(client, message)
+
+
+@PY.UBOT("bokep", sudo=True)
+@PY.TOP_CMD
+async def _(client, message):
+    await _(client, message)
