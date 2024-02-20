734ae9a9c56a27c264b74fc3b9f6d6d92054bab1
Boys_chell <marchellsandreas953@gmail.com>
2024-02-20 10:10:13 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -86,7 +86,7 @@ aiosession = ClientSession()
 
 class Bot(Client):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, device_model="ᴋᴜʟʟ ʙᴇᴛ")
+        super().__init__(**kwargs, device_model="ᴄʜᴇʟʟ ᴜʙᴏᴛ")
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
