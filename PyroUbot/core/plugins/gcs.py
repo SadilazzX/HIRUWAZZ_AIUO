51e520eb5cc04ed52c6fa397d2bd7d97a4dab240
Boys_chell <marchellsandreas953@gmail.com>
2024-02-20 22:12:21 +0700

    Update gcs.py

--- PyroUbot/core/plugins/gcs.py
+++ PyroUbot/core/plugins/gcs.py
@@ -9,25 +9,6 @@ from . import *
 
 __MODULE__ = "broadcast"
 __HELP__ = f"""
-✘ Bantuan Untuk Broadcast
-
-๏ Perintah: <code>{cmd}gucast</code> [text/reply to text/media]
-◉ Penjelasan: Untuk mengirim pesan ke semua user 
-           
-๏ Perintah: <code>{cmd}gcast</code> [text/reply to text/media]
-◉ Penjelasan: Untuk mengirim pesan ke semua group 
-           
-๏ Perintah: <code>{cmd}addbl</code>
-◉ Penjelasan: Menambahkan grup kedalam anti Gcast.
-           
-๏ Perintah: <code>{cmd}delbl</code>
-◉ Penjelasan: Menghapus grup dari daftar anti Gcast.
-           
-๏ Perintah: <code>{cmd}listbl</code>
-◉ Penjelasan: Melihat daftar grup anti Gcast.
-           
-"""
-
 
 @bots.on_message(filters.user(DEVS) & filters.command("cgcast", ".") & ~filters.me)
 @bots.on_message(filters.me & filters.command("gcast", cmd))
