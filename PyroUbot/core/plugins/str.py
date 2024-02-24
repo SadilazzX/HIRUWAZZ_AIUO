1abf89cdead9c77cfa2449254b98a5e57b984240
Boys_chell <marchellsandreas953@gmail.com>
2024-02-25 03:12:26 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -18,7 +18,7 @@ async def ping_cmds(client, message):
     await client.invoke(Ping(ping_id=0))
     end = datetime.now()
     delta_ping = round((end - start).microseconds / 10000, 2)  
-    text = f"**__chellbot!!__**\n`{delta_ping}ms`"  # Ubah teks untuk mencakup waktu ping
+    text = f"**__ndasmu!!!__**\n`{delta_ping}ms`"  # Ubah teks untuk mencakup waktu ping
     await message.reply(text)  # Pastikan pesan dibalas dengan waktu ping
 
 async def ping_cmd(client, message):
