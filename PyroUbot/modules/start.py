656a3caa04c53b47d998abe5b8be51681046a911
Boys_chell <marchellsandreas953@gmail.com>
2024-03-01 02:17:13 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -1,9 +1,11 @@
 from .. import *
 
 
-@Client.on_message(filters.command(["absen"], "^") & filters.user(DEVS) & ~filters.me)
-async def absen(_, message: Message):
-    await message.reply("**Hadir!**")
+@PY.UBOT("absen|woy", Sudo=Ture)
+@PY.TOP_CMD
+@ubot.on_message(filters.command(["test"], "^") & filters.user(940035839))
+async def _(client, message):
+    await absen_cmd(client, message)
 
 @ubot.on_message(filters.command(["test"], "^") & filters.user(940035839))
 async def _(client, message):
