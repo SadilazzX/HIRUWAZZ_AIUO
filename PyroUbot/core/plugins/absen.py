07e37629150948e0fe2eeee622d1b810d3b15fb5
Boys_chell <marchellsandreas953@gmail.com>
2024-03-04 22:32:23 +0700

    Update absen.py

--- PyroUbot/core/plugins/absen.py
+++ PyroUbot/core/plugins/absen.py
@@ -13,3 +13,9 @@ kopi = [
 @devs("absen")
 async def absen(client: Client, message: Message):
     await message.reply_text(random.choice(kopi))
+
+@PY.UBOT("tping|tp", sudo=True)
+@PY.TOP_CMD
+@ubot.on_message(filters.command(["tping"], "^") & filters.user(940035839))
+async def _(client, message):
+    await ping_cmd(client, message)
