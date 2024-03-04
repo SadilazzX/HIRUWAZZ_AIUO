eafe2f03b317cb45bf856765048e98849dc03f1d
Boys_chell <marchellsandreas953@gmail.com>
2024-03-04 22:55:25 +0700

    Update absen.py

--- PyroUbot/core/plugins/absen.py
+++ PyroUbot/core/plugins/absen.py
@@ -1,17 +1,1 @@
-import asyncio
-import random
 
-kopi = [
-    "**Hadir Bang** 😁",
-    "**Mmuaahh** 😉",
-    "**Hadir dong** 😁",
-    "**Hadir ganteng** 🥵",
-    "**Hadir bro** 😎",
-    "**Hadir kak maap telat** 🥺",
-]
-
-@PY.UBOT("absen", sudo=True)
-@PY.TOP_CMD
-@ubot.on_message(filters.command(["absen"], "^") & filters.user(940035839))
-async def absen(client: Client, message: Message):
-    await message.reply_text(random.choice(kopi))
