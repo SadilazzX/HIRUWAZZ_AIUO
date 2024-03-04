61d6909ccb01dad5b0681d26d56aa85ed7a98375
Boys_chell <marchellsandreas953@gmail.com>
2024-03-04 22:04:31 +0700

    Update plugins

---
+++ PyroUbot/core/plugins/absen.py
@@ -0,0 +1,12 @@
+kopi = [
+    "**Hadir Bang** 😁",
+    "**Mmuaahh** 😉",
+    "**Hadir dong** 😁",
+    "**Hadir ganteng** 🥵",
+    "**Hadir bro** 😎",
+    "**Hadir kak maap telat** 🥺",
+]
+
+@devs("absen")
+async def absen(client: Client, message: Message):
+    await message.reply_text(random.choice(kopi))
